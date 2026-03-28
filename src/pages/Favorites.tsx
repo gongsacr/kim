ecd1f39d22705a64284f52b2ab36a8585e3d3f6b
gongsacr <gongsacr@gmail.com>
2026-03-28 16:50:17 +0900

    fix(favorites): Remove outdated and irrelevant list items
    Removes several entries from the movies and animes lists on the Favorites page that are no longer relevant or were duplicates. This cleans up the content presented to the user.

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -4,7 +4,6 @@ import { Film, Book, Tv, User, Gamepad2 } from 'lucide-react';
 export default function FavoritesPage() {
   const movies = [
     { title: '나는 내일 어제의\n너와 만난다', img: 'https://i.postimg.cc/tRSWzY3p/c.webp', comment: '너무슬픔 엉엉슨' },
-    { title: '너와 100번째 사랑', img: 'https://i.postimg.cc/J7XPJKCn/neo.webp', comment: '그냥 거기서 계속 살아' },
     { title: '어바웃타임', img: 'https://i.postimg.cc/T27gcpVG/d.webp', comment: '어떠한 순간을\n다시 살게 된다면' },
     { title: '올드보이', img: 'https://i.postimg.cc/y6QcTkmq/b.webp', comment: '입을 함부로 놀리지 말자' },
     { title: '패밀리맨', img: 'https://i.postimg.cc/8cS1DPGQ/1.webp', comment: '톰행크스 영화는 왜 다 재밌을까' },
@@ -16,14 +15,10 @@ export default function FavoritesPage() {
     { title: '귀를 기울이면', img: 'https://i.postimg.cc/vBPRRz3w/gwi.webp', comment: '별까지 손이 닿도록' },
     { title: '나만이 없는 거리', img: 'https://i.postimg.cc/h4yT0fLJ/나만이_없는_거리.webp', comment: 'OST goat' },
     { title: '날씨의 아이', img: 'https://i.postimg.cc/MKnR3q5d/날씨의_아이.webp', comment: '네가 준 용기라서 널 위해 쓰고 싶어' },
-    { title: '눈동자 속의 암살자', img: 'https://i.postimg.cc/vBK8sbZ3/ko.webp', comment: '좋아하니까!!!(쨍그랑)' },
-    { title: '닥터스톤', img: 'https://i.postimg.cc/br75qSj2/dag.webp', comment: '진짜광기' },
     { title: '데스노트', img: 'https://i.postimg.cc/BQRFdk7h/데스노트.webp', comment: '계획대로' },
     { title: '목소리의 형태', img: 'https://i.postimg.cc/Qxv7RwYv/목소리의_형태_결과.webp', comment: 'す ... き!\nつき?' },
     { title: '몬스터', img: 'https://i.postimg.cc/WbxZQCYH/몬스터.webp', comment: '요한...!' },
     { title: '시간을 달리는 소녀', img: 'https://i.postimg.cc/y6QcTkm6/f.webp', comment: '미래에서 기다릴게' },
-    { title: '암흑마왕 대추적', img: 'https://i.postimg.cc/xjxMPcvC/g.webp', comment: '당신 같은 남자를\n기다려왔다우' },
-    { title: '원펀맨', img: 'https://i.postimg.cc/vmPbbh2t/won.webp', comment: '최강의 남자' },
     { title: '초속 5센티미터', img: 'https://i.postimg.cc/c19wcrQ2/a.webp', comment: 'こんなとこにいる\nはずもないのに' },
     { title: '카구야님은\n고백받고 싶어', img: 'https://i.postimg.cc/htxmgsHt/카구야님.webp', comment: '후지와라 치카;;' },
     { title: '캐릭캐릭 체인지', img: 'https://i.postimg.cc/2SvWmGPL/캐릭캐릭.webp', comment: '아무토마가 진짜다' },
@@ -46,14 +41,14 @@ export default function FavoritesPage() {
     { title: '마이조노 사야카', img: 'https://i.postimg.cc/0NRdnVHc/1c.webp', comment: 'my마이조노' },
     { title: '백인호', img: 'https://i.postimg.cc/GmNP81NC/b3-(1).webp', comment: '난너밖에없어...' },
     { title: '사이하라 슈이치', img: 'https://i.postimg.cc/sxNWfNjh/4.webp', comment: '이런 사이하라랑 시간을 보낼까?' },
-    { title: '세라', img: 'https://i.imghippo.com/files/zTO9093CXA.webp', comment: '천사...' },
+    { title: '세라', img: 'https://i.postimg.cc/GpMMVpxy/se1.webp', comment: '천사...' },
     { title: '시로가네 미유키', img: 'https://i.postimg.cc/2yKWjKkn/3.webp', comment: '세토카이쵸' },
-    { title: '엄', img: 'https://i.imghippo.com/files/xOJW8557YC.webp', comment: '엄' },
+    { title: '엄', img: 'https://i.postimg.cc/y6TJHSS5/eom1.webp', comment: '엄' },
     { title: '이루마 미우', img: 'https://i.postimg.cc/Dzkd6xYp/1d.webp', comment: '토끼모양사과' },
     { title: '체인소', img: 'https://i.postimg.cc/J7gB7zVZ/ㅊ.webp', comment: 'so sexy' },
     { title: '카게야마 토비오', img: 'https://i.postimg.cc/FH4GTZKV/nam1-(1).webp', comment: '결혼하자' },
     { title: '코토부키 츠무기', img: 'https://i.postimg.cc/tJBNcvjz/cheu1.webp', comment: '단무지눈썹' },
-    { title: '파워', img: 'https://i.imghippo.com/files/NM3933Ow.webp', comment: '와시노나와' },
+    { title: '파워', img: 'https://i.postimg.cc/bw9V0gG0/pa1.webp', comment: '와시노나와' },
     { title: 'L', img: 'https://i.postimg.cc/76gxf9F7/3.webp', comment: '저는 XL입니다' },
   ];
 
@@ -62,7 +57,7 @@ export default function FavoritesPage() {
     { title: '단간론파', img: 'https://i.postimg.cc/kgLmjfD7/dan4.webp', comment: '소레와 치가우조' },
     { title: '루미큐브', img: 'https://i.postimg.cc/bdXLc09Y/lu1.webp', comment: '없으면넘겨잇' },
     { title: '마인크래프트', img: 'https://i.postimg.cc/mZqFZgf9/마.webp', comment: '아 멀미나' },
-    { title: '뱀파이어 서바이벌', img: 'https://i.imghippo.com/files/DLiB8688BVI.webp', comment: '뱀서매직' },
+    { title: '뱀파이어 서바이벌', img: 'https://i.postimg.cc/MKy9x5YB/baem.webp', comment: '뱀서매직' },
     { title: '좀비고', img: 'https://i.postimg.cc/xdS0f6QH/nam1.webp', comment: '즐겜해요ㅎㅎ' },
   ];
 
